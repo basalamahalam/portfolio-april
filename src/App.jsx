95e6852fa8ac7788aa94da217db99559e2b2fd71
basalamahalam <basalamahalam@upi.edu>
2025-07-29 14:13:57 +0700

    first commit for some luv

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS Berhasil! 🎉
+      </h1>
+      <p class="text-slate-300 text-3xl mt-10">Dadah</p>
+    </div>
+  );
+}
+
+export default App;
